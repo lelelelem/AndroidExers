876d6f10f1682cde45cf012aca99d7a5ea6a3e31
Lemuel <l.castro@klab.com>
2014-06-10 16:47:37 +0800

    Added Cheat Button and Cheat Activity, calls intent and allow retrieval and sending of extras

--- Book/GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ Book/GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,27 +1,27 @@
 package com.bignerdranch.android.geoquiz;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.R.integer;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.os.Build;
 
 public class QuizActivity extends ActionBarActivity {
 
+	private static final String KEY_INDEX = "index";
+	private static final String IS_CHEATER = "cheater";
+	private boolean mIsCheater;
+	
 	private Button mTrueButton;
 	private Button mFalseButton;
 	
 	private Button mNextButton;
 	private Button mPrevButton;
+	private Button mCheatButton;
 	
 	private TextView mQuestionTextView;
 	
@@ -46,13 +46,26 @@ public class QuizActivity extends ActionBarActivity {
         
         mTrueButton = (Button)findViewById(R.id.true_button);
         mFalseButton = (Button)findViewById(R.id.false_button);
+        mCheatButton = (Button)findViewById(R.id.cheat_button);
        
         mNextButton = (Button)findViewById(R.id.next_button);
         mPrevButton = (Button)findViewById(R.id.prev_button);
         
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(QuizActivity.this, CheatActivity.class);
+				intent.putExtra(CheatActivity.EXTRA_ANSWER, mQuestionBank[mCurrentIndex].isTrueQuestion());
+				startActivityForResult(intent, 0);
+			}
+		});
+        
         mTrueButton.setOnClickListener(new View.OnClickListener() {			
 			@Override
 			public void onClick(View v) {
+				if(mIsCheater){
+					Toast.makeText(QuizActivity.this, R.string.cheater_toast, Toast.LENGTH_SHORT).show();	
+					return;
+				}
 				mChecker(true);
 			}
 		});
@@ -60,6 +73,10 @@ public class QuizActivity extends ActionBarActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {			
 			@Override
 			public void onClick(View v) {
+				if(mIsCheater){
+					Toast.makeText(QuizActivity.this, R.string.cheater_toast, Toast.LENGTH_SHORT).show();	
+					return;
+				}
 				mChecker(false);
 			}
 		});
@@ -78,6 +95,33 @@ public class QuizActivity extends ActionBarActivity {
 			}
 		});
 
+        if (savedInstanceState != null) {
+        	mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        	mIsCheater = savedInstanceState.getBoolean(IS_CHEATER, false);
+       }
+
+        mUpdateQuestion();
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	if (data == null) {
+    	return;
+    	}
+    	mIsCheater = 
+    			data.getBooleanExtra(CheatActivity.HAS_CHEATED, 
+    	false);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+    	super.onSaveInstanceState(savedInstanceState);
+    	savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    	savedInstanceState.putBoolean(IS_CHEATER, mIsCheater);
+    }
+
+    private void mUpdateQuestion(){    	
+    	mQuestionTextView.setText(mQuestionBank[mCurrentIndex].getQuestion());
     }
     
     private void mNextQuestion(){
@@ -85,8 +129,7 @@ public class QuizActivity extends ActionBarActivity {
     	
     	if (mQuestionBank.length==mCurrentIndex+1)
     			Toast.makeText(QuizActivity.this, R.string.end_of_questions, Toast.LENGTH_SHORT).show();
-    	
-		mQuestionTextView.setText(mQuestionBank[mCurrentIndex].getQuestion());
+    	mUpdateQuestion();
     }
     
     private void mPrevQuestion(){
@@ -95,7 +138,7 @@ public class QuizActivity extends ActionBarActivity {
      	if (mCurrentIndex==0)
 			Toast.makeText(QuizActivity.this, R.string.start_of_questions, Toast.LENGTH_SHORT).show();
     	
-    	mQuestionTextView.setText(mQuestionBank[mCurrentIndex].getQuestion());
+    	mUpdateQuestion();
     }
     
     private void mChecker(boolean answer){
@@ -109,25 +152,7 @@ public class QuizActivity extends ActionBarActivity {
     	}
     	
     }
+    
 
- 
-     public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.quiz, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
+    
 }
