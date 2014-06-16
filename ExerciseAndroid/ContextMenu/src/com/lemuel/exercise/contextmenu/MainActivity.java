53587d62cdfaa0013b0c9e1b086fb0ad674265e3
Lemuel <l.castro@klab.com>
2014-06-16 17:17:42 +0800

    Context menu created, Menu is in fragmentMenu

---
+++ ExerciseAndroid/ContextMenu/src/com/lemuel/exercise/contextmenu/MainActivity.java
@@ -0,0 +1,26 @@
+package com.lemuel.exercise.contextmenu;
+
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+
+public class MainActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		android.support.v4.app.FragmentManager fm= getSupportFragmentManager();
+		Fragment fragment=fm.findFragmentById(R.id.container);
+		
+		fragment = new FragmentMenu();
+		fm.beginTransaction().add(R.id.container, fragment).commit();
+		
+		
+	}
+
+	
+
+}
